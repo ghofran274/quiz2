27e4466f6949c0e9e937d8f65ae78cc082b666b7
ghofran274 <gaithkarkeni60@gmail.com>
2024-12-15 10:28:03 +0100

    Add files via upload

---
+++ web2/questions.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+       numb: 1,
+       question: "What does HTML stand for?",
+       image: "img1.jpg",
+       answer: "C. Hyper Text Markup Language",
+       options: [
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language",
+
+       ]     
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        image: "img2.jpg",
+        answer: "A. Cascading Style Sheet",
+        options: [
+          "A. Cascading Style Sheet",
+          "B. Cute Style Sheet",
+          "C. Computer Style Sheet",
+          "D. Codehal Style Sheet",
+        ]     
+     },
+     {
+        numb: 3,
+        question: "What does PHP stand for?",
+        image: "img3.png",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+          "A. Hypertext Preprocessor",
+          "B. Hometext Programming",
+          "C. Hypertext Preprogramming",
+          "D. Programming Hypertext Preprocessor",
+        ]     
+     },
+     {
+        numb: 4,
+        question: "What does SQL stand for?",
+        image: "img4.png",
+        answer: "D. Structured Query Language",
+        options: [
+         "A. Structured Query Language",
+         "B. Structured Query Language",
+         "C. Structured Query Language",
+         "D. Structured Query Language",
+        ]     
+     },
+     {
+        numb: 5,
+        question: "What does XML stand for?",
+        image: "img5.jpg",
+        answer: "D. Extensible Markup Language",
+        options: [
+         "A. Excellent Multiple Language",
+         "B. Explore Multiple Language",
+         "C. Extra Markup Language",
+         "D. Extensible Markup Language",
+        ]     
+     },
+] 
